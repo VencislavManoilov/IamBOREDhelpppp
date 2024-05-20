4599cb7526f1fe7407c9e45a1ce8955326653fec
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-05-20 17:48:12 +0300

    I am having some trouble but I am learning

---
+++ .NET/Simple API/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.MapGet("/", () => {
+    return "<p>Hello World</p>";
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
