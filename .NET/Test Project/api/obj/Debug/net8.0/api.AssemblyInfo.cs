511ec9e38ce2db53ae7c2940adc76871cc93312c
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-06-03 16:46:15 +0300

    I don't know what I did

--- .NET/Test Project/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ .NET/Test Project/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0e39fda39df6fc34183c278446947c18f3f37b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee44c02f00715aca0d3246dff13e35df5c3e1449")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
