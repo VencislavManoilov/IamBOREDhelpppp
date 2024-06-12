75fe551294677411534df675987d8d101841da5d
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-06-12 22:13:19 +0300

    We have a bird; Can move, rotate and draw

--- Boids/Boids/Boids.cpp
+++ Boids/Boids/Boids.cpp
@@ -11,6 +11,40 @@ int main() {
 
 	int time = 0;
 
+	class Bird {
+	public:
+		sf::Vector2f position;
+		float size;
+		float angle;
+
+		Bird(float X, float Y, float Size, float Angle) {
+			position.x = X;
+			position.y = Y;
+			size = Size;
+			angle = Angle;
+
+		}
+
+		void Move() {
+			position.x += std::cos(angle * 3.14/180)/10;
+			position.y += std::sin(angle * 3.14/180)/10;
+		}
+
+		void draw(sf::RenderWindow& window) const {
+			sf::ConvexShape bird;
+			bird.setPointCount(3);
+			bird.setPoint(0, sf::Vector2f(position.x + std::cos((angle + 160) * 3.14/180) * size/2, position.y + std::sin((angle + 160) * 3.14/180) * size/2));
+			bird.setPoint(1, sf::Vector2f(position.x + std::cos((angle - 160) * 3.14/180) * size/2, position.y + std::sin((angle - 160) * 3.14/180) * size/2));
+			bird.setPoint(2, sf::Vector2f(position.x + std::cos((angle) * 3.14/180) * size/2, position.y + std::sin((angle) * 3.14/180) * size/2));
+
+			bird.setFillColor(sf::Color(66, 135, 245));
+
+			window.draw(bird);
+		}
+	};
+
+	Bird bird(100, 100, 50, 0);
+
 	while(window.isOpen()) {
 		while(window.pollEvent(e)) {
 			if (e.type == sf::Event::Closed) {
@@ -25,69 +59,13 @@ int main() {
 			return -1;
 		}
 
-		sf::Vector2i mouseP = sf::Mouse::getPosition();
-		mouseP -= window.getPosition();
+		sf::Vector2i mouseP = sf::Mouse::getPosition(window);
+		 //mouseP -= window.getPosition();
 
 
-		std::string a = "X: " + std::to_string(mouseP.x) + "; Y: " + std::to_string(mouseP.y);
-
-		sf::Text text;
-		text.setFont(font);
-		text.setString(a);
-		text.setCharacterSize(24);
-		text.setFillColor(sf::Color::Red);
-		text.setPosition(10, 10);
-
-		window.draw(text);
-
-
-		sf::CircleShape shape(50.f);
-		shape.setFillColor(sf::Color(150, 50, 250));
-		shape.setPosition(150 + std::cos(time * 3.14 / 180) * 100, 100 + std::sin(time * 3.14 / 180) * 100);
-		shape.setOutlineThickness(10.f);
-		shape.setOutlineColor(sf::Color(250, 150, 100));
-
-		window.draw(shape);
-
-
-		sf::RectangleShape rectangle(sf::Vector2f(120, 50));
-		rectangle.setFillColor(sf::Color::Green);
-		rectangle.setPosition(mouseP.x - 8, mouseP.y - 31);
-
-		window.draw(rectangle);
-
-
-		sf::VertexArray triangle(sf::Triangles, 3);
-
-		triangle[0].position = sf::Vector2f(400, 100.f);
-		triangle[1].position = sf::Vector2f(500.f, 100.f);
-		triangle[2].position = sf::Vector2f(500.f, 200.f);
-
-		triangle[0].color = sf::Color::Red;
-		triangle[1].color = sf::Color::Blue;
-		triangle[2].color = sf::Color::Green;
-
-		window.draw(triangle);
-
-		
-		sf::Texture tex;
-		if (!tex.loadFromFile("images/texture.jpg")) {
-			return -1;
-		}
-
-		sf::RectangleShape player(sf::Vector2f(200, 200));
-		player.setPosition(25, 300);
-		player.setTexture(&tex);
-
-		window.draw(player);
-
-
-		sf::RectangleShape line(sf::Vector2f(5, 100));
-		line.setPosition(400, 400);
-		line.setFillColor(sf::Color::Black);
-		line.rotate(time);
-
-		window.draw(line);
+		bird.angle+=0.1;
+		bird.Move();
+		bird.draw(window);
 
 
 		window.display();
