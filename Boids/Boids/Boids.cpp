fb3c11a24d9c2b9db24a7563703e8f9094777f82
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-06-13 17:46:15 +0300

    OMG!!! It is 90% working!

--- Boids/Boids/Boids.cpp
+++ Boids/Boids/Boids.cpp
@@ -34,48 +34,84 @@ int main() {
 		Bird(float X, float Y, float Size, float Angle)
 		: position(X, Y), size(Size), angle(Angle) {}
 
-		void MoveForward() {
+		void MoveForward(sf::RenderWindow& window) {
 			position.x += std::cos(angle * 3.14/180)/10;
 			position.y += std::sin(angle * 3.14/180)/10;
 
-			position.x = (position.x < -25) ? 825 : position.x;
-			position.x = (position.x > 825) ? -25 : position.x;
-			position.y = (position.y < -25) ? 625 : position.y;
-			position.y = (position.y > 625) ? -25 : position.y;
+			sf::Vector2u windowSize = window.getSize();
+			position.x = (position.x < -25) ? (windowSize.x + 25) : position.x;
+			position.x = (position.x > window.getSize().x + 25) ? -25 : position.x;
+			position.y = (position.y < -25) ? (window.getSize().y + 25) : position.y;
+			position.y = (position.y > window.getSize().y + 25) ? -25 : position.y;
 		}
 
 		void InRadius(Bird birds[], int length) {
 			sf::Vector2f point(0, 0);
-			float Angle = 0;
+			float Angle = 0, moveX = 0, moveY = 0;
 
 			for (int i = 0; i < length; i++) {
+
+				// For Alignment
 				point.x += birds[i].position.x;
 				point.y += birds[i].position.y;
 
+				// For Cohesion
 				Angle += birds[i].angle;
 			}
 
+			moveX /= length;
+			moveY /= length;
 			point.x /= length;
 			point.y /= length;
 			Angle /= length;
 
+			Separation(birds, length, 15);
 			Alignment(Angle);
-			//Cohesion(point.x, point.y);
+			Cohesion(point.x, point.y);
 		}
 
-		void Separation() {
-			// I have no idea what is this
+		void Separation(const Bird* boids, int length, float separationDistance) {
+			float moveX = 0;
+			float moveY = 0;
+			int count = 0;
+
+			for(int i = 0; i < length; i++) {
+				float dx = position.x - boids[i].position.x;
+				float dy = position.y - boids[i].position.y;
+				float distance = std::sqrt(dx * dx + dy * dy);
+
+				if (distance < separationDistance && distance > 0) {
+					moveX += dx / distance;  // Normalized direction vector
+					moveY += dy / distance;  // Normalized direction vector
+					count++;
+				}
+			}
+
+			if (count > 0) {
+				moveX /= count;
+				moveY /= count;
+
+				// Safeguard to ensure no NaN values are used
+				if (!std::isnan(moveX) && !std::isnan(moveY)) {
+					float moveAngle = atan2(moveY, moveX);
+					position.x += std::cos(moveAngle) * 0.1;
+					position.y += std::sin(moveAngle) * 0.1;
+				}
+			}
 		}
 
 		void Alignment(float Angle) {
-			angle = (angle < Angle) ? (angle + 0.1) : (angle - 0.1);
+			if(angle != Angle)
+				angle = (angle < Angle) ? (angle + 0.1) : (angle - 0.1);
 		}
 
 		void Cohesion(float X, float Y) {
-			float angle = atan2(Y - position.y, X - position.x);
+			if (!std::isnan(X) && !std::isnan(Y)) {
+				float angle = atan2(Y - position.y, X - position.x);
 
-			position.x += std::cos(angle)/10;
-			position.y += std::sin(angle)/10;
+				position.x += std::cos(angle)/100;
+				position.y += std::sin(angle)/100;
+			}
 		}
 
 		void draw(sf::RenderWindow& window) const {
@@ -94,7 +130,7 @@ int main() {
 	Bird* birds = new Bird[BirdsNum];
 
 	for(int i = 0; i < BirdsNum; i++) {
-		birds[i] = Bird(randomInteger(25, 775), randomInteger(25, 575), 50, randomInteger(0, 360));
+		birds[i] = Bird(randomInteger(25, 775), randomInteger(25, 575), 25, randomInteger(0, 360));
 	}
 
 	while(window.isOpen()) {
@@ -114,7 +150,7 @@ int main() {
 		sf::Vector2i mouseP = sf::Mouse::getPosition(window);
 
 		for (int i = 0; i < BirdsNum; i++) {
-			birds[i].MoveForward();
+			birds[i].MoveForward(window);
 
 			Bird InRangeBirds[BirdsNum];
 			int length = 0;
