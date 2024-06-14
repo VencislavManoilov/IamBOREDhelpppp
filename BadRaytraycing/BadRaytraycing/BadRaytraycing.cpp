d8f8468eb3bf1a2e203a33f04610a4c5a3fc10ad
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-06-14 22:13:57 +0300

    Added rays

--- BadRaytraycing/BadRaytraycing/BadRaytraycing.cpp
+++ BadRaytraycing/BadRaytraycing/BadRaytraycing.cpp
@@ -34,10 +34,49 @@ int main()
 		}
 	};
 
+	class Ray {
+	public:
+		sf::Vector2f startPosition;
+		float angle;
+		float width;
+		int iterations;
+
+		Ray() : startPosition(0, 0), angle(0), width(0), iterations(0) {}
+
+		Ray(sf::Vector2f StartPosition, float Angle, float Width, int Iterations) :
+		startPosition(StartPosition), angle(Angle), width(Width), iterations(Iterations) {}
+
+		float GetDistance(Box boxes[], int length) {
+
+
+			return 0;
+		}
+
+		void Draw(sf::RenderWindow& window) {
+			std::vector<sf::CircleShape> shape(iterations);
+
+			for (int i = 0; i < iterations; i++) {
+				shape[i].setRadius(2);
+				shape[i].setFillColor(sf::Color::White);
+
+				float dist = i * (width/iterations);
+				shape[i].setPosition(startPosition.x + std::cos(angle * 3.14 / 180) * dist, startPosition.y + std::sin(angle * 3.14 / 180) * dist);
+
+				window.draw(shape[i]);
+			}
+		}
+	};
+
 	Box* boxes = new Box[BoxNum];
+	Ray* rays = new Ray[100];
 
 	for (int i = 0; i < BoxNum; i++) {
 		boxes[i] = Box(sf::Vector2f(randomInteger(0, 700), randomInteger(0, 500)), sf::Vector2f(randomInteger(0, 100), randomInteger(0, 100)), sf::Color::Green);
+	}
+
+	for (int i = 0; i < 100; i++) {
+		float angle = (130. / 99.) * i;
+		rays[i] = Ray(sf::Vector2f(400, 300), angle, 200, 20);
 	}
 
 	while (window.isOpen()) {
@@ -51,6 +90,10 @@ int main()
 
 		for (int i = 0; i < BoxNum; i++) {
 			boxes[i].Draw(window);
+		}
+
+		for (int i = 0; i < 100; i++) {
+			rays[i].Draw(window);
 		}
 
 		window.display();
