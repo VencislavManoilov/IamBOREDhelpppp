56bf8b0fc800a901d5350f8cf3798c068168810f
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-06-16 19:08:08 +0300

    We have falling sand

--- Sand Simulation/Sand Simulation/Sand Simulation.cpp
+++ Sand Simulation/Sand Simulation/Sand Simulation.cpp
@@ -1,13 +1,20 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
+#include <windows.h>
 
 const int SquareSize = 10;
 
 const int SandWidth = 800 / SquareSize;
 const int SandHeight = 600 / SquareSize;
 
-int sand[SandWidth][SandHeight]{};
+std::vector<std::vector<int>> sandStage(SandWidth, std::vector<int>(SandHeight));
+std::vector<std::vector<int>> sandNextStage(SandWidth, std::vector<int>(SandHeight));
 sf::RectangleShape shapes[SandWidth][SandHeight];
+
+static int randomInteger(int min, int max) {
+	return (rand() % (max - min + 1)) + min;
+}
 
 int main()
 {
@@ -16,7 +23,9 @@ int main()
 
 	for (int x = 0; x < SandWidth; x++) {
 		for (int y = 0; y < SandHeight; y++) {
-			sand[x][y] = 0;
+			int stage = randomInteger(0, 1);
+			sandStage[x][y] = stage;
+			sandNextStage[x][y] = stage;
 
 			shapes[x][y].setPosition(x * SquareSize, y * SquareSize);
 			shapes[x][y].setSize(sf::Vector2f(SquareSize, SquareSize));
@@ -33,16 +42,35 @@ int main()
 
 		window.clear(sf::Color::Black);
 
-		sand[10][10] = 1;
+		for (int x = 0; x < SandWidth; x++) {
+			for (int y = 0; y < SandHeight - 1; y++) {
+				if (sandStage[x][y] == 1) {
+					if (sandStage[x][y + 1] != 1) {
+						sandNextStage[x][y] = 0;
+						sandNextStage[x][y + 1] = 1;
+					} else {
+						sandNextStage[x][y] = 1;
+					}
+				}
+			}
+		}
 
 		for (int x = 0; x < SandWidth; x++) {
 			for (int y = 0; y < SandHeight; y++) {
-				if (sand[x][y] == 1) {
+				if (sandStage[x][y] == 1) {
 					window.draw(shapes[x][y]);
 				}
 			}
 		}
 
+		for (int x = 0; x < SandWidth; x++) {
+			for (int y = 0; y < SandHeight; y++) {
+				sandStage[x][y] = sandNextStage[x][y];
+			}
+		}
+
 		window.display();
+
+		Sleep(100);
     }
 }
