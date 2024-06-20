62d32163eb8516c142e1031cc9a767eb99f3cf13
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-06-20 23:05:35 +0300

    Added Drops

--- Paper Marbling/Paper Marbling/Paper Marbling.cpp
+++ Paper Marbling/Paper Marbling/Paper Marbling.cpp
@@ -1,6 +1,31 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+class Drop {
+public:
+	sf::Vector2f position;
+	float R;
+	int points;
+	sf::Color color;
+	sf::ConvexShape shape;
+
+	Drop() : position(0, 0), R(0), points(0), color(0, 0, 0) {}
+
+	Drop(sf::Vector2f Position, float Radius, int Points, sf::Color Color) :
+	position(Position), R(Radius), points(Points), color(Color) {
+		shape.setPointCount(points);
+
+		float angleFromPointToPoint = 360 / points * 3.14 / 180;
+		for (int i = 0; i < points; i++) {
+			shape.setPoint(i, sf::Vector2f(position.x + std::cos(angleFromPointToPoint * i) * R, position.y + std::sin(angleFromPointToPoint * i) * R));
+		}
+
+		shape.setFillColor(color);
+	}
+};
+
+Drop drop = Drop(sf::Vector2f(400, 300), 50, 30, sf::Color::Black);
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Paper Marbling");
@@ -15,7 +40,7 @@ int main()
 
 		window.clear(sf::Color::White);
 
-
+		window.draw(drop.shape);
 
 		window.display();
 	}
