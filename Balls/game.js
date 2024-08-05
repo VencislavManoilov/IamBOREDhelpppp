15f8214b5a4c0a30d814676eeb830ab415a7b0c2
VencislavManoilov <vencislav2.manoilov@gmail.com>
2024-08-05 16:08:47 +0300

    We have working elastic collisions

--- Balls/game.js
+++ Balls/game.js
@@ -1,4 +1,4 @@
-let balls = [], R = 250;
+let balls = [], R = 250, r = 10;
 
 class Ball {
     constructor(x, y, R) {
@@ -20,34 +20,74 @@ class Ball {
     }
 }
 
-balls.push(new Ball(220, 0, 25));
+for(let i = 0; i < 20; i++) {
+    let x = 0;
+    let y = 0;
+    while(distance(250, 250, x, y) > R - r) {
+        x = randomInteger(0, 500);
+        y = randomInteger(0, 500);
+        for(let j = 0; j < balls.length; j++) {
+            if(distance(x, y, balls[j].x, balls[j].y) < r*2) {
+                x = 0; y = 0;
+            }
+        }
+    }
+    balls.push(new Ball(x - 250, y - 250, r))
+}
 
 changeBg("black")
 
 function update() {
-    for(let i = 0; i < balls.length; i++) {
+    for (let i = 0; i < balls.length; i++) {
         // Adds gravity
         balls[i].velocityY += 0.05;
 
         // Checks for border collision
-        if(distance(0, 0, balls[i].x, balls[i].y) > R - balls[i].R) {
-            let normal = Math.atan2(-balls[i].y, -balls[i].x);
-            let angleGoing = Math.atan2(-balls[i].velocityY, -balls[i].velocityX);
+        if (distance(0, 0, balls[i].x, balls[i].y) > R - balls[i].R) {
+            let normal = Math.atan2(balls[i].y, balls[i].x);
+            let angleGoing = Math.atan2(balls[i].velocityY, balls[i].velocityX);
             let difference = normal - angleGoing;
             let angle = normal + difference;
             let speed = distance(0, 0, balls[i].velocityX, balls[i].velocityY);
-            
+
             // Bounces the balls
-            balls[i].velocityX = Math.cos(angle) * speed;
-            balls[i].velocityY = Math.sin(angle) * speed;
+            balls[i].velocityX = -Math.cos(angle) * speed;
+            balls[i].velocityY = -Math.sin(angle) * speed;
         }
 
-        // Checks for balls collision
-        for(let j = 0; j < balls.length; j++) {
-            if(i != j) {
+        // Checks for ball collision
+        for (let j = i + 1; j < balls.length; j++) {
+            let dist = distance(balls[i].x, balls[i].y, balls[j].x, balls[j].y);
+            if (dist < balls[i].R + balls[j].R) {
+                // Calculate the normal and tangent vectors
+                let normalX = (balls[j].x - balls[i].x) / dist;
+                let normalY = (balls[j].y - balls[i].y) / dist;
+                let tangentX = -normalY;
+                let tangentY = normalX;
 
+                // Calculate the dot product of the velocities with the normal and tangent vectors
+                let dotProductNormal1 = balls[i].velocityX * normalX + balls[i].velocityY * normalY;
+                let dotProductNormal2 = balls[j].velocityX * normalX + balls[j].velocityY * normalY;
+                let dotProductTangent1 = balls[i].velocityX * tangentX + balls[i].velocityY * tangentY;
+                let dotProductTangent2 = balls[j].velocityX * tangentX + balls[j].velocityY * tangentY;
+
+                // Calculate the new normal velocities
+                let newDotProductNormal1 = (2 * dotProductNormal2) / 2;
+                let newDotProductNormal2 = (2 * dotProductNormal1) / 2;
+
+                // Update the velocities
+                balls[i].velocityX = tangentX * dotProductTangent1 + normalX * newDotProductNormal1;
+                balls[i].velocityY = tangentY * dotProductTangent1 + normalY * newDotProductNormal1;
+                balls[j].velocityX = tangentX * dotProductTangent2 + normalX * newDotProductNormal2;
+                balls[j].velocityY = tangentY * dotProductTangent2 + normalY * newDotProductNormal2;
+
+                // console.log(dist, normalX, normalY, tangentX, tangentY, dotProductNormal1, dotProductTangent2, newDotProductNormal1, newDotProductNormal2)
             }
         }
+
+        // Update the position based on the velocity
+        balls[i].x += balls[i].velocityX;
+        balls[i].y += balls[i].velocityY;
 
         // Updates the velocity
         balls[i].Update();
